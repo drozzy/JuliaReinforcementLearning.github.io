0a783fdfe9b7ab418d1730887dab5b7780dcb80d
findmyway <find_my_way@foxmail.com>
2020-06-20 14:33:29 +0000

    Deploying to master from  @ 40ea84043c8b4493d1d9d40b34a4bbed3bb59f55 🚀

---
+++ assets/get_started/ex1.jl
@@ -0,0 +1,9 @@
+# This file was generated, do not modify it. # hide
+using ReinforcementLearning # hide
+using Plots
+
+experiment = E`JuliaRL_BasicDQN_CartPole`
+hook = TotalRewardPerEpisode()
+run(experiment.agent, experiment.env, experiment.stop_condition, hook)
+plot(hook.rewards)
+savefig(joinpath(@OUTPUT, "episode.svg")) # hide
---
+++ assets/get_started/ex2.jl
@@ -0,0 +1,11 @@
+# This file was generated, do not modify it. # hide
+using ReinforcementLearning # hide
+using Plots # hide
+
+experiment = E`JuliaRL_BasicDQN_CartPole`
+experiment.agent.policy.learner.γ = 0.98
+hook = TotalRewardPerEpisode()
+
+run(experiment.agent, experiment.env, experiment.stop_condition, hook)
+plot(hook.rewards)
+savefig(joinpath(@OUTPUT, "reward_gamma.svg"))  # hide

d51e6f2f518e6cee3097e8ddcd32db5cc36f3a13
findmyway <find_my_way@foxmail.com>
2021-01-26 03:40:16 +0000

    Deploying to master from  @ 45fbb6fa26f5097434e04f50bf9e01961afc4b65 🚀

--- assets/get_started/env_cart_pole.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-using ReinforcementLearning # hide
-env = CartPoleEnv()
-show(stdout, MIME"text/plain"(), env)  # hide
--- assets/get_started/ex1.jl
+++
@@ -1,9 +0,0 @@
-# This file was generated, do not modify it. # hide
-using ReinforcementLearning # hide
-using Plots # hide
-
-experiment = E`JuliaRL_BasicDQN_CartPole`
-hook = TotalRewardPerEpisode()
-run(experiment.agent, experiment.env, experiment.stop_condition, hook)
-plot(hook.rewards)
-savefig(joinpath(@OUTPUT, "episode.svg")) # hide
